a7b019a9cb81e51d2c3ce4060b7a33477e50d955
Maciej Bednarczyk <m.bednarczyk.1991@gmail.com>
2019-07-03 11:20:14 +0200

    init project

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Calendar from "../components/Calendar/CalendarView";
 import Partners from "../components/Homepage/Partners";
+import News from "../components/Homepage/News";
 import "../assets/styles/Homepage.scss";
 
 export default function Homepage() {
     return (
         <div className="home-page container">
             <Partners />
+            <News />
             <h2>Kalendarz</h2>
             <Calendar />
         </div>
